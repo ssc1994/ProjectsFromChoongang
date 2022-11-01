717bb233cbbc662028868965ea4a4a6d9ce83657
user <user@DESKTOP-81MKMOM>
2022-11-01 15:24:56 +0900

    11_01
    Project Start

---
+++ MovieProject/src/com/project/movie/Login.java
@@ -0,0 +1,9 @@
+package com.project.movie;
+
+public class Login {//회원가입한 고객 클래스
+	 private String name; //사용자 이름
+	 private String birth; //생년월일
+	 private String id; //아이디
+	 private String pw; //비밀번호
+
+}
---
+++ MovieProject/src/com/project/movie/Theater.java
@@ -0,0 +1,9 @@
+package com.project.movie;
+
+public class Theater {//현재 상영중인 영화
+	 private Movie movie; //영화 정보
+	 private int round; //회차 정보
+	 private String start; //시작시간 
+	 private String finish; //끝시간
+	 private boolean reserve; //예매여부 
+}
