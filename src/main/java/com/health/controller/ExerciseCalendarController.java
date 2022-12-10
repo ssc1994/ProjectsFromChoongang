23c2c449a52f20024ebfb0bf74b64c0812fb05b4
tg0910 <wjdxorud456@naver.com>
2022-12-10 19:40:23 +0900

    1210카페커밋

--- src/main/java/com/health/controller/ExerciseCalendarController.java
+++ src/main/java/com/health/controller/ExerciseCalendarController.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 
 
 @WebServlet("*.Calendar")
-public class ExerciseCalendar extends HttpServlet {
+public class ExerciseCalendarController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
