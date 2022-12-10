308db201c9887bf7e71633087fb4ebd5074477c7
tg0910 <wjdxorud456@naver.com>
2022-12-10 19:31:44 +0900

    ggg

---
+++ src/main/java/com/health/exerciseinfo/model/ExerciseinfoDAO.java
@@ -0,0 +1,58 @@
+package com.health.exerciseinfo.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.health.util.UtilClose;
+
+public class ExerciseinfoDAO {
+	
+	private static ExerciseinfoDAO instance = new ExerciseinfoDAO(); 
+
+	public ExerciseinfoDAO() {
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (Exception e) {
+			System.out.println("드라이버 클래스 로드 에러");
+		}
+		
+	}
+
+	public static ExerciseinfoDAO getInstance() {
+		return instance;
+	}
+	
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	
+	public static final String URL = "jdbc:oracle:thin:@172.30.1.39:1521:xe";
+	public static final String UID = "health";
+	public static final String UPW = "health";
+	
+	public void ExInsert(String exeno1,String exeno2,String exeno3,String exeno4,String exeno5,String exeno6,String id,String routine) {
+	
+		String sql = inse
+		
+		try {
+			conn = DriverManager.getConnection(URL, UID, UPW);
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.set
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			UtilClose.close(conn, pstmt, rs);
+		}
+		
+	}
+	
+
+}
