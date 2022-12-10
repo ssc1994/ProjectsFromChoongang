0f952d4d3f4b8acf32a80dcbc8c52cc870f18546
Youngjae-Joo <joo0jae@gmail.com>
2022-12-10 19:27:45 +0900

    주영재커밋

--- src/main/java/com/health/routine/model/RoutineDAO.java
+++ src/main/java/com/health/routine/model/RoutineDAO.java
@@ -1,6 +1,11 @@
 package com.health.routine.model;
 
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 import com.health.util.UtilClose;
 
 public class RoutineDAO {
@@ -22,26 +27,34 @@ public class RoutineDAO {
 		return instance;
 	}
 	
-	public static final String URL = "jdbc:oracle:thin:@172.30.1.5:1521:xe";
+	public static final String URL = "jdbc:oracle:thin:@172.30.1.39:1521:xe";
 	public static final String UID = "health";
 	public static final String UPW = "health";
 	
-	public int getRoutine(int idno) {
-		
-		int result = 0;
-		String sql ="";
-		
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	
+	public int getRoutine(int rno) {
+		String sql ="select rno from routine where rno=?";
+		 int result = 0;
 		try {
-		
+			conn=DriverManager.getConnection(URL, UID, UPW);
+			
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setInt(1, rno);
+			
+			rs=pstmt.executeQuery();
+			
+			if(rs.next()) {
+				result = rs.getInt("rno");
+			}
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}finally {
-			UtilClose.close(null, null, null);
+			UtilClose.close(conn, pstmt, rs);
 		}
-		
-		
-		
 		return result;
 	}
 	
